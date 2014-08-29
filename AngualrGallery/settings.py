b1d750e3740df28d773d472884f68541b27e5fdb
stock-site <stock-site@ip-50-63-147-106.ip.secureserver.net>
2014-08-28 19:40:31 -0600

    Removed TODO, updating settings.

--- AngualrGallery/settings.py
+++ AngualrGallery/settings.py
@@ -51,7 +51,7 @@ else:
     }
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['secure.sianware.com', 'gallerydev.sianware.com']
+ALLOWED_HOSTS = ['panda.gnoek.com', 'secure.sianware.com', 'gallerydev.sianware.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
